a044b46377569b9889493f81418b225f1899a774
thangampalraj <136328968+thangampalraj@users.noreply.github.com>
2023-07-04 16:58:14 +0100

    Create linux-script.sh

---
+++ linux-script.sh
@@ -0,0 +1,10 @@
+          echo what is installed in it?
+          java -version
+          ant -version
+          ls -a
+          whoami
+          env
+          hostname
+          pwd
+          free
+          df -h
